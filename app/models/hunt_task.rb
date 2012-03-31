a6de362ab819bd4ff138b2aacc221a33125ab09b
Ben Downey <BD@BenDowney.net>
2012-03-31 18:33:44 -0500

    more HuntTask fun

--- app/models/hunt_task.rb
+++ app/models/hunt_task.rb
@@ -1,6 +1,4 @@
-    class HuntTask < ActiveRecord::Base
-      attr_accessible :name
-       
-      belongs_to :hunt # the id for the association is in this table
-      belongs_to :task
-    end
+class HuntTask < ActiveRecord::Bas   
+  belongs_to :hunt # the id for the association is in this table
+  belongs_to :task
+end
