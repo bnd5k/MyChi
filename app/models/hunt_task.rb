115fbf991331e0f0e12ef7ac06952e5e95e3c301
Ben Downey <BD@BenDowney.net>
2012-04-06 13:39:36 -0500

    little progress on hunt-tasks

--- app/models/hunt_task.rb
+++ app/models/hunt_task.rb
@@ -1,4 +1,5 @@
-class HuntTask < ActiveRecord::Bas   
-  belongs_to :hunt # the id for the association is in this table
-  belongs_to :task
+class HuntTask < ActiveRecord::Base
+  belongs_to :hunts
+  belongs_to :tasks
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,10 @@
 class Task < ActiveRecord::Base
-  
-  has_many :hunt_tasks
-  has_many :hunts, :through => :hunt_tasks
-  attr_accessible :name
  
+  belongs_to :hunts 
+  attr_accessible :name 
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 } ,
                     :uniqueness => { :case_sensitive => false }
+ 
 
 end
