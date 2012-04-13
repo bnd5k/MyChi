054e742c4e20c7bb31e8c163ff5b72e991ae8454
Ben Downey <BD@BenDowney.net>
2012-04-13 08:56:35 -0500

    added email addy to splash page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,11 @@
-    class ApplicationController < ActionController::Base
-      protect_from_forgery
-      include SessionsHelper
-     
-# before_filter :redirect_to_root_path
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  include SessionsHelper
 
-#def redirect_to_root_path
-#  redirect_to root_path
-#end
+  before_filter(:except => [:splash, :create]) do
+    redirect_to root_path
+  end
 
-
- before_filter(:except => [:splash, :create]) do
-   redirect_to root_path
- end
- 
- #root_path # pages_splash_path #root_path "pages#splash"
- 
-# before_filter redirect_to root_path
- 
- ## def handle_unverified_request
-  #  raise ActionController::InvalidAuthenticityToken
-  #end    
+   
   
 end
