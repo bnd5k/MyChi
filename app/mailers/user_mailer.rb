7527cb79fc90a2e01f43a3f868b4068d794df4ed
Ben Downey <BD@BenDowney.net>
2012-05-03 18:36:21 -0500

    removed token b.s. form invitaiton functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,21 +9,8 @@ class UserMailer < ActionMailer::Base
   end
   
   def invitation(invitation)
-
-    url_path = invitation_url(invitation.token)
- # @user = user
-#    subject    'Invitation'
- #   recipients invitation.recipient_email
-    #body       :invitation => invitation, :signup_url => url_path
-    #body "You have been invited"
- 
     invitation.update_attribute(:sent_at, Time.now)
     mail(:to => invitation.recipient_email, :subject => "You're invited", :body => "Welcome")
-
-
-
   end
-#"Welcome to MyChi!  #{friend user.name} has invited you to to a hunt. 
-  #      Follow this link to get started! #{url_path}  "
         
 end
