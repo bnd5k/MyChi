d863b978a8746b767a4b4f0b286e25bb986c9a46
Ben Downey <BD@BenDowney.net>
2012-05-19 09:37:23 -0500

    using rake db:seed file now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development?
+  Hunt.destroy_all
+  Task.destroy_all
+  HuntTask.destroy_all
+
+  #city_hunt = Hunt.create name: 'Scour the City'
+  hood_hunt = Hunt.create name: 'Best of Ukranian Villiage'
+
+
+open("db/hood_hunt.txt") do |tasks|
+    tasks.read.each_line do |row|
+      name, location, points = row.chomp.split("|")
+      hood_hunt.tasks.create!(:name => name, :location => location, :points => points)
+    end
+  end
+  puts "Rails dev db ready for use"
+end
