58dc0d55852337e94dab071b2be683738c599608
Ben Downey <BD@BenDowney.net>
2012-05-20 17:05:28 -0500

    revised seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ if Rails.env.development?
 
   # make_users
     admin = User.create!(:name => "Ben",
-                         :email => "bd@bendowney.net",
+                         :email => "Ben@mychi.com",
                          :password => "foobar",
                          :password_confirmation => "foobar")
     admin.toggle!(:admin)
